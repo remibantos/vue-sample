5e124117ffef22c1a2e7588cd20fe0d1f1a1361a
remibantos <remi.bantos@gmail.com>
2018-04-11 00:40:28 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <b-button>{{buttonName}}</b-button>
+  </div>
+</template>
+
+<script>
+import bButton from 'bootstrap-vue/es/components/button/button'
+export default {
+  name: 'HelloWorld',
+  components: {bButton},
+  data () {
+    return {
+      buttonName: 'Hello Button'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
