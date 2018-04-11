2659a24ddb9dd2b98efdae6d43649c6b8ab9a09f
rbantos <rbantos@egencia.com>
2018-04-11 15:23:57 +0200

    Use Vue test-utils

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
+import { shallow } from '@vue/test-utils'
+import bButton from 'bootstrap-vue/es/components/button/button'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
+
+    const wrapper = shallow(HelloWorld, {stubs: [bButton]})
+    console.log(wrapper.html())
+    const vm = wrapper.vm
     expect(vm.$el.querySelector('.hello button').textContent)
       .toEqual('Hello Button')
   })
